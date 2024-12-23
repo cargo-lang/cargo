cfa761980e5f6d0b54264a7a2bc68e11249f17f1
DevByEagle <142937112+DevByEagle@users.noreply.github.com>
2024-12-23 02:38:31 +0000

    Updated code

--- include/lexer.h
+++ include/lexer.h
@@ -1,1 +1,16 @@
-#ifndef 
+#ifndef LEXER_H
+#define LEXER_H
+
+#include <string>
+#include <vector>
+
+namespace Cargo {
+    struct Token {
+        std::string type;
+        std::string value;
+    };
+
+    std::vector<Token> tokenize(const std::string& source);
+}
+
+#endif // LEXER_H
---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <fstream>
+#include <iostream>
+#include <unordered_map>
+#include "../include/lexer.h"
+//#include "../include/parser.h"
+//#include "../include/codegen.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " <source>" << std::endl;
+        return 1;
+    }
+}
